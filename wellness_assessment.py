b03437a8fa91996c683259852a14c733551a77d7
kremeth <66464637+kremeth@users.noreply.github.com>
2024-02-04 10:00:30 +0800

    Update wellness_assessment.py

--- wellness_assessment.py
+++ wellness_assessment.py
@@ -2,6 +2,7 @@ import requests
 from flask import Flask, request
 import json
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
@@ -9,126 +10,126 @@ url = 'https://c50fca.myshopify.com'
 access_token= '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'
 
 
-def create_session():
-    s=requests.Session()
-    s.headers.update({
-        "X-Shopify-Access-Token": access_token,
-        "Content-Type" : "application/json"
-    })
-    return s
+# def create_session():
+#     s=requests.Session()
+#     s.headers.update({
+#         "X-Shopify-Access-Token": access_token,
+#         "Content-Type" : "application/json"
+#     })
+#     return s
 
 
-def get_customer_id(email):
-    sess = create_session()
-    search_url = url + "/admin/api/2024-01/customers/search.json?query=email:"+email
-    try:
-        resp = sess.get(search_url)
-        resp.raise_for_status()  # This will raise an exception for HTTP errors.
-        #print(resp.json())
-        return resp.json(), 200  # Return the parsed JSON data and status code.
-    except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
-        print(e)
-        return {"error": str(e)}, 500
+# def get_customer_id(email):
+#     sess = create_session()
+#     search_url = url + "/admin/api/2024-01/customers/search.json?query=email:"+email
+#     try:
+#         resp = sess.get(search_url)
+#         resp.raise_for_status()  # This will raise an exception for HTTP errors.
+#         #print(resp.json())
+#         return resp.json(), 200  # Return the parsed JSON data and status code.
+#     except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
+#         print(e)
+#         return {"error": str(e)}, 500
     
     
-def get_product_id(name):
-    sess = create_session()
-    search_url = url + "/admin/api/2024-01/products.json?title="+name
-    try:
-        resp = sess.get(search_url)
-        resp.raise_for_status()  # This will raise an exception for HTTP errors.
-        #print(resp.json())
-        return resp.json(), 200  # Return the parsed JSON data and status code.
-    except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
-        print(e)
-        return {"error": str(e)}, 500
+# def get_product_id(name):
+#     sess = create_session()
+#     search_url = url + "/admin/api/2024-01/products.json?title="+name
+#     try:
+#         resp = sess.get(search_url)
+#         resp.raise_for_status()  # This will raise an exception for HTTP errors.
+#         #print(resp.json())
+#         return resp.json(), 200  # Return the parsed JSON data and status code.
+#     except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
+#         print(e)
+#         return {"error": str(e)}, 500
     
 
-@app.route("/customer-id/<string:customer_email>", methods=["GET"])
-def get_customer(customer_email):
-    # Construct the URL for the POST request
-    resp, status = get_customer_id(customer_email)
-    return resp, status
+# @app.route("/customer-id/<string:customer_email>", methods=["GET"])
+# def get_customer(customer_email):
+#     # Construct the URL for the POST request
+#     resp, status = get_customer_id(customer_email)
+#     return resp, status
 
 
-@app.route("/get-product/<string:product_name>", methods=["GET"])
-def get_product(product_name):
-    product, status_product = get_product_id(product_name)
-    print(product)
-    productId = product["products"][0]["id"]
-    #get_url = url + "/admin/api/2024-01/metafields.json?metafield['owner_id']="+str(customerId)
-    #get_url = url + "/admin/api/2024-01/metafields.json?metafield[namespace]=quiz_analysis"
-    get_url = url + "/admin/api/2024-01/products/"+str(productId)+"/images.json"
-    #get_url = url + "/admin/api/2024-01/customers/"+str(customerId)+"/metafields.json"
-    try:
-        sess = create_session()
-        response = sess.get(get_url)
-        response.raise_for_status()  # This will raise an exception for HTTP errors.
-        for image in response.json()['images']:
-            if image['position']==1:
-                resp=image
-        #print(resp.json())
-        return resp, 200  # Return the parsed JSON data and status code.ß
-    except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
-        print(e)
-        return {"error": str(e)}, 500
+# @app.route("/get-product/<string:product_name>", methods=["GET"])
+# def get_product(product_name):
+#     product, status_product = get_product_id(product_name)
+#     print(product)
+#     productId = product["products"][0]["id"]
+#     #get_url = url + "/admin/api/2024-01/metafields.json?metafield['owner_id']="+str(customerId)
+#     #get_url = url + "/admin/api/2024-01/metafields.json?metafield[namespace]=quiz_analysis"
+#     get_url = url + "/admin/api/2024-01/products/"+str(productId)+"/images.json"
+#     #get_url = url + "/admin/api/2024-01/customers/"+str(customerId)+"/metafields.json"
+#     try:
+#         sess = create_session()
+#         response = sess.get(get_url)
+#         response.raise_for_status()  # This will raise an exception for HTTP errors.
+#         for image in response.json()['images']:
+#             if image['position']==1:
+#                 resp=image
+#         #print(resp.json())
+#         return resp, 200  # Return the parsed JSON data and status code.ß
+#     except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
+#         print(e)
+#         return {"error": str(e)}, 500
     
     
     
-@app.route("/get_quiz_analysis/<string:customer_email>", methods=["GET"])
-def get_quiz_analysis(customer_email):
-    customer, status_customer = get_customer_id(customer_email)
-    customerId = customer["customers"][0]["id"]
-    #get_url = url + "/admin/api/2024-01/metafields.json?metafield['owner_id']="+str(customerId)
-    #get_url = url + "/admin/api/2024-01/metafields.json?metafield[namespace]=quiz_analysis"
-    get_url = url + "/admin/api/2024-01/metafields.json?metafield[owner_id]="+str(customerId)+"&metafield[owner_resource]=customer&metafield[namespace]=quiz_analysis"
-    #get_url = url + "/admin/api/2024-01/customers/"+str(customerId)+"/metafields.json"
-    try:
-        sess = create_session()
-        response = sess.get(get_url)
-        response.raise_for_status()  # This will raise an exception for HTTP errors.
-        #print(resp.json())
-        return response.json(), 200  # Return the parsed JSON data and status code.ß
-    except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
-        print(e)
-        return {"error": str(e)}, 500
+# @app.route("/get_quiz_analysis/<string:customer_email>", methods=["GET"])
+# def get_quiz_analysis(customer_email):
+#     customer, status_customer = get_customer_id(customer_email)
+#     customerId = customer["customers"][0]["id"]
+#     #get_url = url + "/admin/api/2024-01/metafields.json?metafield['owner_id']="+str(customerId)
+#     #get_url = url + "/admin/api/2024-01/metafields.json?metafield[namespace]=quiz_analysis"
+#     get_url = url + "/admin/api/2024-01/metafields.json?metafield[owner_id]="+str(customerId)+"&metafield[owner_resource]=customer&metafield[namespace]=quiz_analysis"
+#     #get_url = url + "/admin/api/2024-01/customers/"+str(customerId)+"/metafields.json"
+#     try:
+#         sess = create_session()
+#         response = sess.get(get_url)
+#         response.raise_for_status()  # This will raise an exception for HTTP errors.
+#         #print(resp.json())
+#         return response.json(), 200  # Return the parsed JSON data and status code.ß
+#     except requests.RequestException as e:  # This catches HTTP errors and other Request exceptions.
+#         print(e)
+#         return {"error": str(e)}, 500
 
 
-@app.route("/update-metafield/<string:customer_email>/<string:date>", methods=["POST"])
-def update_metafield(customer_email,date):
-        # Get data from the request's body
-    request_data = request.get_json()
+# @app.route("/update-metafield/<string:customer_email>/<string:date>", methods=["POST"])
+# def update_metafield(customer_email,date):
+#         # Get data from the request's body
+#     request_data = request.get_json()
 
-    # Create a session with headers
-    sess = create_session()
+#     # Create a session with headers
+#     sess = create_session()
 
-    # Construct the URL for the POST request
-    customer, status = get_customer_id(customer_email)
-    print(customer["customers"])
-    customerId = customer["customers"][0]["id"]
+#     # Construct the URL for the POST request
+#     customer, status = get_customer_id(customer_email)
+#     print(customer["customers"])
+#     customerId = customer["customers"][0]["id"]
     
-    post_url = url + "/admin/api/2024-01/customers/"+str(customerId)+"/metafields.json"
+#     post_url = url + "/admin/api/2024-01/customers/"+str(customerId)+"/metafields.json"
 
-    # Prepare the data for the metafield update
-    metafield_data = {
-        "metafield": {
-            "namespace": "quiz_analysis",
-            "key": date,
-            "value": json.dumps(request_data["value"]),
-            "type": "json_string",  # Make sure the type matches the type of the value
-        }
-    }
+#     # Prepare the data for the metafield update
+#     metafield_data = {
+#         "metafield": {
+#             "namespace": "quiz_analysis",
+#             "key": date,
+#             "value": json.dumps(request_data["value"]),
+#             "type": "json_string",  # Make sure the type matches the type of the value
+#         }
+#     }
 
-    # Make the POST request
-    response = sess.post(post_url, json=metafield_data)
+#     # Make the POST request
+#     response = sess.post(post_url, json=metafield_data)
 
-    # Check the response status and return the result
-    if response.status_code == 200 or response.status_code == 201:
-        # If the request was successful, return the JSON response from Shopify
-        return response.json(), 200
-    else:
-        # If the request failed, return the status code and error message
-        return {"error": response.text}, response.status_code
+#     # Check the response status and return the result
+#     if response.status_code == 200 or response.status_code == 201:
+#         # If the request was successful, return the JSON response from Shopify
+#         return response.json(), 200
+#     else:
+#         # If the request failed, return the status code and error message
+#         return {"error": response.text}, response.status_code
     
     
 CORS(app)
